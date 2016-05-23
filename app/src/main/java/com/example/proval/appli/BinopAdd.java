ac8eb9c62502288a3f3a14ff321373938395dce7
SushiEleonore <eleonore_bartenlian@yahoo.fr>
2016-05-23 10:00:49 +0200

    Added BinopAdd & BinopMult

---
+++ app/src/main/java/com/example/proval/appli/BinopAdd.java
@@ -0,0 +1,11 @@
+package com.example.proval.appli;
+
+/**
+ * Created by proval on 5/23/16.
+ */
+public class BinopAdd extends Binop {
+
+    BinopAdd(Expr e1, Expr e2) {
+        super (e1, e2);
+    }
+}
---
+++ app/src/main/java/com/example/proval/appli/BinopMult.java
@@ -0,0 +1,11 @@
+package com.example.proval.appli;
+
+/**
+ * Created by proval on 5/23/16.
+ */
+public class BinopMult extends Binop {
+
+    BinopMult(Expr e1, Expr e2) {
+        super (e1, e2);
+    }
+}
